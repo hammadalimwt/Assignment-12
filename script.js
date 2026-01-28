b85e6b142bff3ccb1a4fe025729e7955b6d5945a
hammadalimwt <hammadalimwt@gmail.com>
2026-01-28 17:32:30 +0500

    added all files

---
+++ script.js
@@ -0,0 +1,21 @@
+// Step 3: Hello World messages
+alert("Hello World");
+console.log("Hello World");
+
+// Step 4: Variables declaration
+var studentName = "Hammad Ali";       // var variable
+let courseName = "Web Development";   // let variable
+const batchNumber = "Batch 1 Morning"; // const variable
+
+// Step 5: Display variables in console
+console.log(studentName);
+console.log(courseName);
+console.log(batchNumber);
+
+document.write(studentName) // variable value on the webpage
+
+courseName = "MERN Stack Development";
+console.log(courseName);
+
+batchNumber = "Batch 2 Morning"; // error in console
+console.log(batchNumber);
